18f93f366b7c335f12c047d0c34d4fa15bd26116
yusuf20090 <tzcan07@gmail.com>
2023-02-13 15:51:02 +0300

    item3

--- js/script.js
+++ js/script.js
@@ -5,23 +5,23 @@ const swiper = new Swiper('.menu-slider', {
 let menuList = [
 
   menu__item = {
-    img: '../img/menu 3,.jpeg',
+    img: '../img/menu1.jpeg',
     h3: 'Tatlı Ekşi Soslu Tavuk',
     p: 'Baharatlı basmati pirinci, Sebzeli tavuk sote ve Brokoli ile',
     span: '175₺',
   },
 
   menu__item = {
-    img: '../img/meni 4.png',
+    img: '../img/menu2.png',
     h3: 'Piliç Pikata',
     p: 'Limon soslu ve paneli tavuk göğüs, bakliyat salatası ile',
     span: '120₺',
   },
   menu__item = {
-    img: '3',
-    h3: 'kavurma',
-    p: '',
-    span: '',
+    img: '../img/menu3.jpeg',
+    h3: 'Antrikot Izgara',
+    p: 'Izgara Dana Antrikot, Fırın patates ve Demi glace sos ile',
+    span: '150₺',
   },
 ]
 console.log(
@@ -32,6 +32,7 @@ let divMenuList = document.querySelector('.menu__list');
 let divMenuItem = divMenuList.querySelector('.menu__item');
 for (let i = 0; i < menuList.length; i++) {
   let divMenuItemClone = divMenuItem.cloneNode(true);
+  divMenuItemClone.classList.remove('simple');
   divMenuItemClone.querySelector('img').setAttribute('src', menuList[i].img)
   divMenuItemClone.querySelector('h3').innerText = menuList[i].h3;
   divMenuItemClone.querySelector('p').innerText = menuList[i].p;
