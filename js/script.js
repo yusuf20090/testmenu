1cb9dde6727511b6a7c06271afd61d03337ab4d3
yusuf20090 <tzcan07@gmail.com>
2023-02-20 16:02:50 +0300

    add store

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 const swiper = new Swiper('.menu-slider', {
   speed: 400,
   spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.4,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2.3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2.6,
+      spaceBetween: 30
+    }
+  }
 });
-let menuList = [
+let menuListAnayemek = [
 
   menu__item = {
     img: '../img/menu1.jpeg',
@@ -24,26 +41,103 @@ let menuList = [
     span: '150₺',
   },
 ]
-console.log(
-  menuList[0]
-)
 let divMenuList = document.querySelector('.menu__list');
-
 let divMenuItem = divMenuList.querySelector('.menu__item');
-for (let i = 0; i < menuList.length; i++) {
+for (let i = 0; i < menuListAnayemek.length; i++) {
   let divMenuItemClone = divMenuItem.cloneNode(true);
   divMenuItemClone.classList.remove('simple');
-  divMenuItemClone.querySelector('img').setAttribute('src', menuList[i].img)
-  divMenuItemClone.querySelector('h3').innerText = menuList[i].h3;
-  divMenuItemClone.querySelector('p').innerText = menuList[i].p;
-  divMenuItemClone.querySelector('span').innerText = menuList[i].span;
+  divMenuItemClone.querySelector('img').setAttribute('src', menuListAnayemek[i].img)
+  divMenuItemClone.querySelector('h3').innerText = menuListAnayemek[i].h3;
+  divMenuItemClone.querySelector('p').innerText = menuListAnayemek[i].p;
+  divMenuItemClone.querySelector('span').innerText = menuListAnayemek[i].span;
   divMenuList.appendChild(divMenuItemClone);
 }
-// divMenuItem.setAttribute('class','menu__item');
-// let img = divMenuItem.createElement('img');
-// img.setAttribute('src','');
-// let divMenuInfo =divMenuItem.createElement('div')
-// divMenuInfo.setAttribute('class','menu__info');
-// let h3 = divMenuInfo.createElement('h3');
-// let p = divMenuInfo.createElement('p');
-// let span = divMenuInfo.createElement('span');
+let AnayemekButton=document.querySelector('#Anayemek');
+AnayemekButton.onclick=function(){
+  divMenuList.innerHTML='';
+  for (let i = 0; i < menuListAnayemek.length; i++) {
+    let divMenuItemClone = divMenuItem.cloneNode(true);
+    divMenuItemClone.classList.remove('simple');
+    divMenuItemClone.querySelector('img').setAttribute('src', menuListAnayemek[i].img)
+    divMenuItemClone.querySelector('h3').innerText = menuListAnayemek[i].h3;
+    divMenuItemClone.querySelector('p').innerText = menuListAnayemek[i].p;
+    divMenuItemClone.querySelector('span').innerText = menuListAnayemek[i].span;
+    divMenuList.appendChild(divMenuItemClone);
+  }
+}
+
+
+
+let menuListAperatif = [
+
+  menu__item = {
+    img: '../img/menuAperatif1.jpeg',
+    h3: 'Bitterballen',
+    p: 'Tütsülenmiş dana brisket dolgulu, Hollanda usulü çıtır misket köfteler, ballı hardal sos ile',
+    span: '100₺',
+  },
+
+  menu__item = {
+    img: '../img/menuAperatif2.jpeg',
+    h3: 'Soğan Halkası',
+    p: 'burada açiklamasi bulunuyor',
+    span: '20₺',
+  },
+  menu__item = {
+    img: '../img/menuAperatif3.jpeg',
+    h3: 'Peynir Mozarella Sticks',
+    p: 'burada açiklamasi bulunuyor',
+    span: '25₺',
+  },
+]
+let aperatifButton=document.querySelector('#aperatif');
+aperatifButton.onclick=function(){
+  divMenuList.innerHTML='';
+  for (let i = 0; i < menuListAperatif.length; i++) {
+    let divMenuItemClone = divMenuItem.cloneNode(true);
+    divMenuItemClone.classList.remove('simple');
+    divMenuItemClone.querySelector('img').setAttribute('src', menuListAperatif[i].img)
+    divMenuItemClone.querySelector('h3').innerText = menuListAperatif[i].h3;
+    divMenuItemClone.querySelector('p').innerText = menuListAperatif[i].p;
+    divMenuItemClone.querySelector('span').innerText = menuListAperatif[i].span;
+    divMenuList.appendChild(divMenuItemClone);
+  }
+}
+
+
+
+let menuListİçecekler = [
+
+  menu__item = {
+    img: '../img/menuİçecek1.jpeg',
+    h3: 'Orman Meyveli Limonata',
+    p: 'burada açiklamasi bulunuyor',
+    span: '60₺',
+  },
+
+  menu__item = {
+    img: '../img/menuİçecek2.jpeg',
+    h3: 'Bubblegum Milkshake',
+    p: 'Bubblegum, dondurma',
+    span: '45₺',
+  },
+  menu__item = {
+    img: '../img/menuİçecek3.jpeg',
+    h3: 'Blueberry Bubble Berry Punch Ice Tea',
+    p: 'burada açiklamasi bulunuyor',
+    span: '50₺',
+  },
+]
+let İçeceklerButton=document.querySelector('#İçecekler');
+İçeceklerButton.onclick=function(){
+  divMenuList.innerHTML='';
+  for (let i = 0; i < menuListAperatif.length; i++) {
+    let divMenuItemClone = divMenuItem.cloneNode(true);
+    divMenuItemClone.classList.remove('simple');
+    divMenuItemClone.querySelector('img').setAttribute('src', menuListİçecekler[i].img)
+    divMenuItemClone.querySelector('h3').innerText = menuListİçecekler[i].h3;
+    divMenuItemClone.querySelector('p').innerText = menuListİçecekler[i].p;
+    divMenuItemClone.querySelector('span').innerText = menuListİçecekler[i].span;
+    divMenuList.appendChild(divMenuItemClone);
+  }
+}
